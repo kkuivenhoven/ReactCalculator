55b027f9fa63887e13312405ba843a2fe20dadc1
Kendra Kuivenhoven <kkuivenhoven@mail.csuchico.edu>
2017-08-06 10:11:14 -0700

    Basic hello react message up and running

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+// import React and Component from the react package
 import React, { Component } from 'react';
+// import Text and AppRegistry from the react-native package
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	Text,
+	AppRegistry
 } from 'react-native';
 
-export default class ReactCalculator extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
+// Registers ReactCalculator with the AppRegistry imported above
 AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
+
+// definition of our ReactCalculator component
+// a component is essentially a view that can contain
+// subviews and sub-components
+class ReactCalculator extends Component {
+	
+	render() {
+		return (
+			<Text>Hello, React!</Text>
+		)
+	}
+
+}
